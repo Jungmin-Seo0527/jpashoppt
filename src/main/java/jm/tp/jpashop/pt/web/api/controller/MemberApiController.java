4b99c847ceecf1b6612b2056e43dffbf446c2212
Jungmin-Seo0527 <soato1405@naver.com>
2021-07-21 00:20:27 +0900

    회원 가입 API controller 수정
    ApiResult 클래스로 ResponseBody 객체를 한번 감싸주었다. 이를 통해 데이터에 대한 사이즈, 에러 메시지 등을 json에 추가할 수 있는 확장성을 가졌다.

--- src/main/java/jm/tp/jpashop/pt/web/api/controller/MemberApiController.java
+++ src/main/java/jm/tp/jpashop/pt/web/api/controller/MemberApiController.java
@@ -1,6 +1,7 @@
 package jm.tp.jpashop.pt.web.api.controller;
 
 import jm.tp.jpashop.pt.service.MemberService;
+import jm.tp.jpashop.pt.web.api.dto.ApiResult;
 import jm.tp.jpashop.pt.web.api.dto.MemberApiDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.http.HttpStatus.ACCEPTED;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @RestController
@@ -28,8 +28,29 @@ public class MemberApiController {
         try {
             memberService.join(memberApiDto);
         } catch (IllegalStateException e) {
-            return new ResponseEntity<>(memberApiDto, BAD_REQUEST);
+            return ResponseEntity.status(BAD_REQUEST).body(memberApiDto);
         }
-        return new ResponseEntity<>(memberApiDto, ACCEPTED);
+        return ResponseEntity.ok(memberApiDto);
+    }
+
+    @PostMapping("/api/member3")
+    public ApiResult<MemberApiDto> joinMember3(@RequestBody MemberApiDto memberApiDto) {
+        try {
+            memberService.join(memberApiDto);
+        } catch (IllegalStateException e) {
+            return ApiResult.failed(memberApiDto, "중복하는 회원이 존재합니다.");
+        }
+        return ApiResult.succeed(memberApiDto);
+    }
+
+    @PostMapping("/api/member4")
+    public ResponseEntity<ApiResult<MemberApiDto>> joinMember4(@RequestBody MemberApiDto memberApiDto) {
+        try {
+            memberService.join(memberApiDto);
+        } catch (IllegalStateException e) {
+            return ResponseEntity.status(BAD_REQUEST)
+                    .body(ApiResult.failed(memberApiDto, "이름이 중복되는 회원이 존재합니다."));
+        }
+        return ResponseEntity.ok(ApiResult.succeed(memberApiDto));
     }
 }
